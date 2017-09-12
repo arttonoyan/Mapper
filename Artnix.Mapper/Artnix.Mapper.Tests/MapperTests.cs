b84b644b36bff1f2079da9345be9b89a32cfceeb
Artyom Tonoyan <arttonoyan@gmail.com>
2017-09-12 10:14:05 +0400

    Added "TestEnumerableModelToEnumerableViewModel" TestMethod.

--- Artnix.Mapper/Artnix.Mapper.Tests/MapperTests.cs
+++ Artnix.Mapper/Artnix.Mapper.Tests/MapperTests.cs
@@ -16,19 +16,49 @@ namespace Artnix.Mapper.Tests
             Mapper.MapConfiguration(cfg =>
             {
                 cfg.CreateMap<Mock.Models.StudentModelMock, Mock.MapModels.Base.StudentModelMock>()
-                    .Property(wm => wm.CityName, m => m.CityModel.Name)
+                // You can use "IfNotNull" extensions.
+                .Property(wm => wm.CityName, m => m.CityModel.IfNotNull(p => p.Name))
                     .Property(wm => wm.Fullname, m => $"{m.Family} {m.Name}");
 
                 cfg.CreateMap<Mock.Models.StudentModelMock, Mock.MapModels.Nullable.StudentModelMock>()
-                    .Property(wm => wm.CityName, m => m.CityModel.Name)
+                    .Property(wm => wm.CityName, m => m.CityModel == null ? null : m.CityModel.Name)
                     .Property(wm => wm.Fullname, m => $"{m.Family} {m.Name}");
             });
         }
 
         [TestMethod]
-        public void TestMethodToViewModel()
+        public void TestModelToViewModel()
         {
-            
+            var items = new List<Mock.MapModels.Base.StudentModelMock>();
+            var Converter = Mapper.Converter<Mock.Models.StudentModelMock, Mock.MapModels.Base.StudentModelMock>();
+            foreach (var stItem in DataBase.Students)
+            {
+                try
+                {
+                    Mock.MapModels.Base.StudentModelMock mapModel = Converter(stItem);
+                    items.Add(mapModel);
+                }
+                catch (Exception e)
+                {
+                    Assert.Fail(e.Message);
+                }
+            }
+
+            Assert.IsTrue(items.Count == DataBase.Students.Count);
+        }
+
+        [TestMethod]
+        public void TestEnumerableModelToEnumerableViewModel()
+        {
+            try
+            {
+                var models = DataBase.Students.MapConvert<Mock.Models.StudentModelMock, Mock.MapModels.Base.StudentModelMock>().ToList();
+                Assert.IsTrue(models.Count == DataBase.Students.Count);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
         }
     }
 }
